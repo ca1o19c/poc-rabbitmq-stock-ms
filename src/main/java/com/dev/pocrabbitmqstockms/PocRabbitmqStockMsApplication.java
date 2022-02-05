0595930abfaf57a20c9b674858b0a71c01334940
Caio Antonio <caio.antonio.c@outlook.com>
2022-02-04 21:24:06 -0300

    project init

---
+++ src/main/java/com/dev/pocrabbitmqstockms/PocRabbitmqStockMsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.pocrabbitmqstockms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRabbitmqStockMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocRabbitmqStockMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/pocrabbitmqstockms/PocRabbitmqStockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.pocrabbitmqstockms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocRabbitmqStockMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
